18b939cbd25194f439979ca62b8ddfc992e5707e
michal <michk100@gmail.com>
2021-05-24 20:47:05 +0000

    pandaenv

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo test
+useradd -u 1030 artifactory
+chown -R artifactory:artifactory ./volumes/artifactory
+chown -R 1000:1000 ./volumes/jenkins
+cd compose
+docker-compose up -d --build
